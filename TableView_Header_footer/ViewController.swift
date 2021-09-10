dd5b4f51fde3ee8b1d558f9a912b3c7a6b16838a
rahimBasha <rahimdev786@gmail.com>
2021-09-10 18:02:39 +0530

    this header and footer data

--- TableView_Header_footer/ViewController.swift
+++ TableView_Header_footer/ViewController.swift
@@ -7,13 +7,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+var data = [["one","two","three","four","sizteen"],["banana","apple","Custoer"],["cricket","resource"]]
+var headerTitle = ["First","second","Third"]
 
+    @IBOutlet weak var Table1:UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Table1.delegate = self
+        Table1.dataSource = self
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height:200))
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 150))
+        
+        headerView.backgroundColor = .blue
+        footerView.backgroundColor = .cyan
+        
+        Table1.tableHeaderView = headerView
+        Table1.tableFooterView = footerView
     }
-
-
+ 
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Table1.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.section][indexPath.row]
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        Table1.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        return headerTitle[section]
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
 }
-
