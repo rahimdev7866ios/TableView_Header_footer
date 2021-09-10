de627ac45113ba621b7bde6b13fa5ebe1e9ee7ec
rahimBasha <rahimdev786@gmail.com>
2021-09-10 17:03:55 +0530

    Initial Commit

---
+++ TableView_Header_footer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Header_footer
+//
+//  Created by shaik Arshad on 10/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
